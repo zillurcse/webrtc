4dd8fd28f8ddea2e159ec5bc4e8d97a2833eca3c
zillurcse <zillurrahman68582@gmail.com>
2023-10-16 23:32:13 +0600

    ok

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 let config = {
-    chanalName:'meetease',
-    token:'007eJxTYIiIPG4qondns6ru+RiHyvwo9ZYjM6y/3V36yJup/Gzv1bMKDAZJyUZGRiZmiYmGRiYGqckW5iYGackmKaappomGBmYm/nuVUhsCGRmeZrOxMjJAIIjPwZCbmlqSmlicysAAAKrIIHY=',
-    appCertificate:'de6b5334701545e5bfc1169fe6aa5cb0',
-    appId:'0bc22246aa1240ec8740fc4d5e5a1064'
+    chanalName:'channel-meet42',
+    token:'0062a87519765ef40fea9b20dad323928d0IABd7anJuSg\/dd7Y\/Ld7iAaOgsYg9PGgc92OQoVr0dtnxqzbE\/0fuyeOIgC6NQAA+sQuZQQAAQCKgS1lAwCKgS1lAgCKgS1lBACKgS1l',
+    appCertificate:'49856fe243474ded9f61f2d714ecd8eb',
+    appId:'2a87519765ef40fea9b20dad323928d0'
 }
 console.log(config);
